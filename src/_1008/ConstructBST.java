2bf85d34a63b78c609eeae50a28880587eacdc35
tinglu <lisalu729@gmail.com>
2019-04-02 23:42:52 +1100

    1008. Construct Binary Search Tree from Preorder Traversal - faster solution

--- src/_1008/ConstructBST.java
+++ src/_1008/ConstructBST.java
@@ -15,33 +15,49 @@ class TreeNode {
 }
 
 public class ConstructBST {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        Stack<TreeNode> nodes = new Stack<>();
+    //    Faster solution
+    int i = 0;
 
-        if (preorder.length == 0) return null;
-
-        int prevVal = preorder[0];
-        TreeNode rootNode = new TreeNode(prevVal);
-        nodes.push(rootNode);
-
-        if (preorder.length > 1) {
-            for (int i = 1; i < preorder.length; i++) {
-                TreeNode currNode = new TreeNode(preorder[i]);
-
-                if (preorder[i] < prevVal) {
-                    nodes.peek().left = currNode;
-                } else {
-                    TreeNode parent = nodes.peek();
-                    while (!nodes.empty() && preorder[i] > nodes.peek().val) {
-                        parent = nodes.pop();
-                    }
-                    parent.right = currNode;
-                }
-                nodes.push(currNode);
-
-                prevVal = preorder[i];
-            }
-        }
-        return rootNode;
+    public TreeNode bstFromPreorder(int[] A) {
+        return bstFromPreorder(A, Integer.MAX_VALUE);
     }
+
+    public TreeNode bstFromPreorder(int[] A, int bound) {
+        if (i == A.length || A[i] > bound) return null;
+        TreeNode root = new TreeNode(A[i++]);
+        root.left = bstFromPreorder(A, root.val);
+        root.right = bstFromPreorder(A, bound);
+        return root;
+    }
+
+//    Mine
+//    public TreeNode bstFromPreorder(int[] preorder) {
+//        Stack<TreeNode> nodes = new Stack<>();
+//
+//        if (preorder.length == 0) return null;
+//
+//        int prevVal = preorder[0];
+//        TreeNode rootNode = new TreeNode(prevVal);
+//        nodes.push(rootNode);
+//
+//        if (preorder.length > 1) {
+//            for (int i = 1; i < preorder.length; i++) {
+//                TreeNode currNode = new TreeNode(preorder[i]);
+//
+//                if (preorder[i] < prevVal) {
+//                    nodes.peek().left = currNode;
+//                } else {
+//                    TreeNode parent = nodes.peek();
+//                    while (!nodes.empty() && preorder[i] > nodes.peek().val) {
+//                        parent = nodes.pop();
+//                    }
+//                    parent.right = currNode;
+//                }
+//                nodes.push(currNode);
+//
+//                prevVal = preorder[i];
+//            }
+//        }
+//        return rootNode;
+//    }
 }
