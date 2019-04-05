b431fa26f13c5f34f67c6ef402bfd7111841f0f9
tinglu <lisalu729@gmail.com>
2019-04-05 13:21:03 +1100

    update utils

--- src/Utils/MyPrint.java
+++ src/Utils/MyPrint.java
@@ -1,16 +1,25 @@
+package Utils;
+
 import java.util.Set;
 
-public class Util {
-    void printSet(Set<int[]> results) {
+public class MyPrint {
+    public static void printSet(Set<int[]> results) {
         for (int[] p : results) {
             System.out.print(String.format("[%d,%d]", p[0], p[1]));
         }
         System.out.println("");
     }
 
-    void printArray(int[][] A) {
+    public static void print2DArray(int[][] A) {
         for (int[] p : A) {
             System.out.print(String.format("[%d,%d]", p[0], p[1]));
+        }
+        System.out.println("");
+    }
+
+    public static void printArray(int[] A) {
+        for (int d : A) {
+            System.out.print(String.format("%d, ", d));
         }
         System.out.println("");
     }
