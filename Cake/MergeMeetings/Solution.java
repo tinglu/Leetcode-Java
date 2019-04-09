95a18283d661465a865fff1148d863ab5d7a0e8e
tinglu <lisalu729@gmail.com>
2019-04-09 23:47:50 +1000

    Cake: MergeMeetingTimes

---
+++ Cake/MergeMeetings/Solution.java
@@ -0,0 +1,206 @@
+package MergeMeetings;
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Solution {
+
+    public static class Meeting {
+
+        private int startTime;
+        private int endTime;
+
+        public Meeting(int startTime, int endTime) {
+            // number of 30 min blocks past 9:00 am
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public int getStartTime() {
+            return startTime;
+        }
+
+        public void setStartTime(int startTime) {
+            this.startTime = startTime;
+        }
+
+        public int getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(int endTime) {
+            this.endTime = endTime;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            }
+            if (!(o instanceof Meeting)) {
+                return false;
+            }
+            final Meeting meeting = (Meeting) o;
+            return startTime == meeting.startTime && endTime == meeting.endTime;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = 17;
+            result = result * 31 + startTime;
+            result = result * 31 + endTime;
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", startTime, endTime);
+        }
+    }
+
+    public static List<Meeting> mergeRanges(List<Meeting> meetings) {
+        // merge meetings ranges
+        if (meetings.size() < 1) return null;
+
+        List<int[]> slots = new ArrayList<>();
+        int maxSlot = Integer.MIN_VALUE;
+
+        for (Meeting meeting : meetings) {
+            int[] slot = {meeting.startTime, meeting.endTime};
+            slots.add(slot);
+            if (meeting.endTime > maxSlot) maxSlot = meeting.endTime;
+        }
+
+        Comparator<int[]> comparator = (int[] o1, int[] o2) -> o1[0] - o2[0];
+        Collections.sort(slots, comparator);
+
+        for (int[] part : slots) {
+            System.out.print(Arrays.toString(part) + ", ");
+        }
+        System.out.println("");
+
+        List<int[]> merged = new ArrayList<>();
+        int from = slots.get(0)[0];
+        int to = slots.get(0)[1];
+        for (int i = 1; i < slots.size(); i++) {
+            if (slots.get(i)[0] <= to) {
+                to = slots.get(i)[1] > to ? slots.get(i)[1] : to;
+            } else {
+                int[] slot2 = {from, to};
+                merged.add(slot2);
+                from = slots.get(i)[0];
+                to = slots.get(i)[1];
+            }
+            if (i == slots.size() - 1) {
+                int[] slot2 = {from, to};
+                merged.add(slot2);
+            }
+        }
+
+        List<Meeting> result = new ArrayList<>();
+        for (int[] slot : merged) {
+            Meeting meeting = new Meeting(slot[0], slot[1]);
+            result.add(meeting);
+        }
+        return result;
+    }
+
+
+    // tests
+
+    @Test
+    public void meetingsOverlapTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 3), new Meeting(2, 4));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 4));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingsTouchTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(5, 6), new Meeting(6, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(5, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingContainsOtherMeetingTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 8), new Meeting(2, 5));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingsStaySeparateTest() {
+        final List<Meeting> meetings = Arrays.asList(new Meeting(1, 3), new Meeting(4, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 3), new Meeting(4, 8)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multipleMergedMeetingsTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(1, 4), new Meeting(2, 5), new Meeting(5, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(new Meeting(1, 8));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void meetingsNotSortedTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(5, 8), new Meeting(1, 4), new Meeting(6, 8));
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 4), new Meeting(5, 8)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneLongMeetingContainsSmallerMeetingsTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(1, 10), new Meeting(2, 5), new Meeting(6, 8),
+                new Meeting(9, 10), new Meeting(10, 12)
+        );
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(1, 12)
+        );
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sampleInputTest() {
+        final List<Meeting> meetings = Arrays.asList(
+                new Meeting(0, 1), new Meeting(3, 5), new Meeting(4, 8),
+                new Meeting(10, 12), new Meeting(9, 10)
+        );
+        final List<Meeting> actual = mergeRanges(meetings);
+        final List<Meeting> expected = Arrays.asList(
+                new Meeting(0, 1), new Meeting(3, 8), new Meeting(9, 12)
+        );
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
