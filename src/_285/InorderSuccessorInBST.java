d8223daaa43bc09df21bc695ef45a9689ed08ead
tinglu <lisalu729@gmail.com>
2019-04-22 10:32:23 +1000

    285. Inorder Successor in BST

---
+++ src/_285/InorderSuccessorInBST.java
@@ -0,0 +1,68 @@
+package _285;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class InorderSuccessorInBST {
+    // Recursion: Don't understand this solution
+//    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+//        if (root == null) return null;
+//
+//        if (root.val <= p.val) {
+//            return inorderSuccessor(root.right, p);
+//        } else {
+//            TreeNode left = inorderSuccessor(root.left, p);
+//            return left == null ? root : left;
+//        }
+//    }
+
+    // Iterative way
+    // TODO: review later
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode succ = null;
+
+        while (root != null) {
+            if (root.val <= p.val) {
+                root = root.right;
+            } else {
+                succ = root;
+                root = root.left;
+            }
+        }
+        return succ;
+    }
+
+//    XXX Wrong!
+//    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+////        if (root == null || p == null) return null;
+////        if (root.val == p.val && root.right != null) return root.right;
+////
+////        if (root.left != null && root.left.val == p.val) return root;
+////        if (root.val > p.val) return inorderSuccessor(root.left, p);
+////
+////        if (root.val < p.val) return inorderSuccessor(root.right, p);
+////
+////        return null;
+//        return helper(root, p, root);
+//    }
+//
+//    private TreeNode helper(TreeNode root, TreeNode p, TreeNode max) {
+//        if (root == null || p == null) return null;
+//        if (root.val == p.val && root.right != null) return root.right;
+//
+//        if (root.left != null && root.left.val == p.val) return root;
+//        if (root.val > p.val) return helper(root.left, p, root);
+//
+//        if (root.right != null && root.right.val == p.val) return max;
+//        if (root.val < p.val) return helper(root.right, p, null);
+//
+//        return null;
+//    }
+}
