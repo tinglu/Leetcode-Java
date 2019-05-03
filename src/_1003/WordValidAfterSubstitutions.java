f63d254cbce80f45698dfe09cd87aeec42691f24
tinglu <lisalu729@gmail.com>
2019-05-03 21:40:00 +1000

    1003. Check If Word Is Valid After Substitutions

---
+++ src/_1003/WordValidAfterSubstitutions.java
@@ -0,0 +1,39 @@
+package _1003;
+
+import java.util.Stack;
+
+public class WordValidAfterSubstitutions {
+
+    public boolean isValid(String S) {
+
+        char[] letters = S.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < letters.length; i++) {
+            char curr = letters[i];
+            switch (curr) {
+                case 'a':
+                    stack.add(curr);
+                    break;
+                case 'b':
+                    if (i == letters.length - 1) return false;
+                    if (letters[i + 1] == 'c' && !stack.empty() && stack.peek() == 'a') {
+                        stack.pop();
+                        i++;
+                    } else {
+                        stack.add(curr);
+                    }
+                    break;
+                case 'c':
+                    if (stack.empty() || stack.peek() != 'b') return false;
+                    stack.pop();
+                    if (stack.empty() || stack.peek() != 'a') return false;
+                    stack.pop();
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return stack.empty();
+    }
+}
