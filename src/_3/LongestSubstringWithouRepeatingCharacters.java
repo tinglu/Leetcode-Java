7a05498438a111b7a7359898a5b43e031f73f4f1
tinglu <lisalu729@gmail.com>
2019-05-11 21:13:38 +1000

    faster 3. Longest Substring Without Repeating Characters

--- src/_3/LongestSubstringWithouRepeatingCharacters.java
+++ src/_3/LongestSubstringWithouRepeatingCharacters.java
@@ -1,33 +1,34 @@
 package _3;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubstringWithouRepeatingCharacters {
 
-    public int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        char[] letters = s.toCharArray();
-
-        for (int i = 0; i < letters.length; i++) {
-            Set<Character> exists = new HashSet<>();
-
-            for (int j = i; j < letters.length; j++) {
-
-                if (exists.contains(letters[j])) {
-                    max = Math.max(max, j - i);
-                    break;
-                } else {
-                    if (j == letters.length - 1) {
-//                        System.out.println("i: " + i + " " + letters[i] + " - j: " + j + " " + letters[j]);
-                        max = Math.max(max, j - i + 1);
-                    }
-                    exists.add(letters[j]);
-                }
-            }
-        }
-        return max;
-    }
+    // Did in April 2019
+//    public int lengthOfLongestSubstring(String s) {
+//        int max = 0;
+//        char[] letters = s.toCharArray();
+//
+//        for (int i = 0; i < letters.length; i++) {
+//            Set<Character> exists = new HashSet<>();
+//
+//            for (int j = i; j < letters.length; j++) {
+//
+//                if (exists.contains(letters[j])) {
+//                    max = Math.max(max, j - i);
+//                    break;
+//                } else {
+//                    if (j == letters.length - 1) {
+////                        System.out.println("i: " + i + " " + letters[i] + " - j: " + j + " " + letters[j]);
+//                        max = Math.max(max, j - i + 1);
+//                    }
+//                    exists.add(letters[j]);
+//                }
+//            }
+//        }
+//        return max;
+//    }
 
     public static void main(String[] args) {
         LongestSubstringWithouRepeatingCharacters solution = new LongestSubstringWithouRepeatingCharacters();
@@ -35,5 +36,33 @@ public class LongestSubstringWithouRepeatingCharacters {
         System.out.println(solution.lengthOfLongestSubstring("bbbbb"));
         System.out.println(solution.lengthOfLongestSubstring("pwwkew"));
         System.out.println(solution.lengthOfLongestSubstring("bbbbbc"));
+    }
+
+    // Re-do in May 2019
+    public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        char[] letters = s.toCharArray();
+
+        Map<Character, Integer> charToIdx = new HashMap<>();
+        int start = 0;
+        for (int i = start; i < letters.length; i++) {
+            if (charToIdx.containsKey(letters[i])) {
+                max = Math.max(max, i - start);
+
+                // Get previous index of the duplicated letter
+                int prevIdx = charToIdx.get(letters[i]);
+                // Remove all letters before this dup letter and itself from the map
+                for (int j = start; j <= prevIdx; j++) {
+                    charToIdx.remove(letters[j]);
+                }
+                // Map this dup letter to the new index
+                charToIdx.put(letters[i], i);
+                start = prevIdx + 1;
+            } else {
+                charToIdx.put(letters[i], i);
+                max = Math.max(max, i - start + 1);
+            }
+        }
+        return max;
     }
 }
