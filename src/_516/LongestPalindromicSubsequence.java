6a43d95ba7e1dcca2580598d1d33317fa81fc6c9
tinglu <lisalu729@gmail.com>
2019-06-17 11:26:40 +1000

    516. Longest Palindromic Subsequence [DONE]

--- src/_516/LongestPalindromicSubsequence.java
+++ src/_516/LongestPalindromicSubsequence.java
@@ -3,10 +3,10 @@ package _516;
 
 // TODO
 /*
-*
-* Dynamic programming
-*
-* */
+ *
+ * Dynamic programming
+ *
+ * */
 public class LongestPalindromicSubsequence {
 
 
@@ -39,31 +39,26 @@ public class LongestPalindromicSubsequence {
 //    }
 
 
-    public int longestPalindromeSubseq(String s) {
-        int result = 0;
+    private int helper(int from, int to, String s, int[][] memo) {
+        if (memo[from][to] != 0) return memo[from][to];
 
-        int len = s.length();
-        if (len == 0) return 0;
-        if (len == 1) return 1;
+        if (from == to) return 1;
+        if (from > to) return 0;
 
-        int left = 0;
-        int right = len - 1;
-
-        while (left < right) {
-
-            for (int j = right; j > left; j--) {
-                if (s.charAt(j) == s.charAt(left)) {
-                    result += 2;
-                    right = j - 1;
-                    break;
-                }
-            }
-
-            left++;
+        if (s.charAt(from) == s.charAt(to)) {
+            memo[from][to] = helper(from + 1, to - 1, s, memo) + 2;
+        } else {
+            memo[from][to] = Math.max(helper(from, to - 1, s, memo), helper(from + 1, to, s, memo));
         }
 
-        if (left <= right) result += 1; // has some characters left in-between the palindromic letter
-        return result;
+        return memo[from][to];
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        int len = s.length();
+
+        int[][] memo = new int[len][len];
+        return helper(0, len - 1, s, memo);
     }
 
     public static void main(String[] args) {
@@ -72,5 +67,6 @@ public class LongestPalindromicSubsequence {
         System.out.println(sol.longestPalindromeSubseq("cbbd"));
         System.out.println(sol.longestPalindromeSubseq("a"));
         System.out.println(sol.longestPalindromeSubseq("ab"));
+        System.out.println(sol.longestPalindromeSubseq("aabaaba"));
     }
 }
