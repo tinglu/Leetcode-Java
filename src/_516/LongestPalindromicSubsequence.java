4b3a2ec395d2d5fb8d17a0c2c915bc88e6300dd4
tinglu <lisalu729@gmail.com>
2019-06-17 11:55:13 +1000

    647. Palindromic Substrings

--- src/_516/LongestPalindromicSubsequence.java
+++ src/_516/LongestPalindromicSubsequence.java
@@ -1,7 +1,6 @@
 package _516;
 
 
-// TODO
 /*
  *
  * Dynamic programming
--- src/_647/PalindromicSubstrings.java
+++ src/_647/PalindromicSubstrings.java
@@ -1,30 +1,65 @@
 package _647;
 
 // TODO
+
+
 public class PalindromicSubstrings {
-    public boolean isPalindrome(String s, int from, int to) {
-        while (from < to) {
-            if (s.charAt(from) == s.charAt(to)) {
-                from++;
-                to--;
-            } else {
-                return false;
-            }
-        }
 
-        return true;
-    }
+    /*
+     *
+     * This approach takes more than O(N^2) complexity
+     *
+     * */
+//    public boolean isPalindrome(String s, int from, int to) {
+//        while (from < to) {
+//            if (s.charAt(from) == s.charAt(to)) {
+//                from++;
+//                to--;
+//            } else {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//
+//
+//    public int countSubstrings(String s) {
+//        int len = s.length();
+//        int result = 0;
+//
+//        for (int i = 0; i < len; i++) {
+//            for (int j = i; j < len; j++) {
+//                if (isPalindrome(s, i, j)) {
+//                    result += 1;
+//                }
+//            }
+//        }
+//        return result;
+//    }
 
 
+    /*
+     *
+     * O(N^2) time - expand from centre
+     * O(1) space
+     *
+     * */
     public int countSubstrings(String s) {
-        int len = s.length();
+        int N = s.length();
         int result = 0;
-
-        for (int i = 0; i < len; i++) {
-            for (int j = i; j < len; j++) {
-                if (isPalindrome(s, i, j)) {
-                    result += 1;
-                }
+        for (int center = 0; center < 2 * N - 1; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < N && s.charAt(left) == s.charAt(right)) {
+                /*
+                 *
+                 * !!!!!! while is very important!! need to expand!!!
+                 *
+                 * */
+                result += 1;
+                left--;
+                right++;
             }
         }
         return result;
