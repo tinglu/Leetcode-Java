714f6a4cc4d2189e8c2e05677c96bb74310569d2
tinglu <lisalu729@gmail.com>
2019-06-20 17:42:57 +1000

    cake: revise 5 - graph coloring

--- Cake/TreesAndGraphs/GraphColouring.java
+++ Cake/TreesAndGraphs/GraphColouring.java
@@ -55,6 +55,23 @@ public class GraphColouring {
      *
      * O(N+M) time where N is the number of nodes and M is the number of edges.
      *
+     * The runtime might not look linear because we have outer and inner loops. The trick is to look at each step and
+     * think of things in terms of the total number of edges (M) wherever we can:
+     *
+     * We check if each node appears in its own hash set of neighbors.
+     * Checking if something is in a hash set is O(1), so doing it for all N nodes is O(N).
+     *
+     * When we get the illegal (used) colors for each node, we iterate through that node's neighbors. So in total, we
+     * cross each of the graphs M edges twice: once for the node on either end of each edge. O(2*M) --> O(M) time.
+     *
+     * When we assign a color to each node, we're careful to stop checking colors as soon as we find one that works.
+     * In the worst case, we'll have to check one more color than the total number of neighbors. Again, each edge in
+     * the graph adds two neighbors — one for the node on either end—so there are 2*M neighbors. So, in total, we'll
+     * have to try O(N(1 + k)) -> 1 is for extra color if all neigbours are assigned colors --> O(N + N*k) --> O(N + M)
+     * colors.
+     *
+     * Putting all the steps together, our complexity is O(N + 2M + N+M) --> O(N+M).
+     *
      *
      * The only thing we're storing is the illegalColors hash set. In the worst case, all the neighbors of a node
      * with the maximum degree (D) have different colors, so our hash set takes up O(D) space.
